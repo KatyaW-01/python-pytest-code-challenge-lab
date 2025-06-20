26b57da012ba151af3eea1f21c954e0f1d2572c4
Katya Weicht <katya.weicht@gmail.com>
2025-06-20 11:40:01 -0600

    Make assert statements for example inputs

--- lib/testing/test_palindrome.py
+++ lib/testing/test_palindrome.py
@@ -1,2 +1,8 @@
+from lib.palindrome import longest_palindromic_substring
+
 def test_longest_palindromic_substring(s):
-  
+  assert longest_palindromic_substring("babad") in ["bab", "aba"]
+  assert longest_palindromic_substring("cbbd") == "bb"
+  assert longest_palindromic_substring("a") == "a"
+  assert longest_palindromic_substring("ac") in ["a", "c"]
+  assert longest_palindromic_substring("racecar") == "racecar"

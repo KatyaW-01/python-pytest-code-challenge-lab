4bb51a1ab6ed4f722109c04a8395683c1eccc376
Katya Weicht <katya.weicht@gmail.com>
2025-06-20 12:14:29 -0600

    Add edge cases and check input conditions

--- lib/testing/test_palindrome.py
+++ lib/testing/test_palindrome.py
@@ -1,8 +1,26 @@
 from lib.palindrome import longest_palindromic_substring
 
-def test_longest_palindromic_substring(s):
+def test_longest_palindromic_substring():
+  inputs = ["babad", "cbbd", "a", "ac", "racecar"]
+  for s in inputs: #check all inputs for length and only numbers/letters
+    assert len(s) <= 1000 and len(s) >= 1
+    assert s.isalnum()
+
   assert longest_palindromic_substring("babad") in ["bab", "aba"]
   assert longest_palindromic_substring("cbbd") == "bb"
   assert longest_palindromic_substring("a") == "a"
   assert longest_palindromic_substring("ac") in ["a", "c"]
   assert longest_palindromic_substring("racecar") == "racecar"
+
+  #edge cases
+  assert longest_palindromic_substring("") == ""
+  assert longest_palindromic_substring("abcd") == "a"
+  test_string = (
+    "thequickbrownfoxjumpsoverthelazydogmadamimadamwenttothestore"
+    "andboughtsomebananasforlunchbeforeheadinghomewithapalindromemadamracecar"
+    "tacocatciviclevelrotorstatsdeifiedrepaperneveroddoreven"
+  )
+  assert longest_palindromic_substring(test_string) in ["madamimadam","madam", "racecar","tacocat","civic","level","rotor","stats","deified","repaper","neveroddoreven"]
+
+
+  
